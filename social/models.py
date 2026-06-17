e18a99f7c2b0ace395ef7973833e9711c05d6715
shanti <240719491+Sanny-lama@users.noreply.github.com>
2026-06-17 21:20:01 +0545

    added like system feature

--- social/models.py
+++ social/models.py
@@ -1,119 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Interaction
+#feeback page like system
+from social.models import Post
 
 
-class Post(models.Model):
+class like(models.Model):
+ user = models.Foreignkey(User, on_delete=models.CASCADE)
+ post = models.Foreignkey(Post, on_delete=models.CASDCADE)
 
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    content = models.TextField()
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-
-    def __str__(self):
-        return self.content[:20]
-
-class Like(models.Model):
-
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-
-    class Meta:
-        unique_together = ('user','post')
-
-
-
-
-class Comment(models.Model):
-
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE
-    )
-
-    text = models.TextField()
-
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-
-
-
-class Follow(models.Model):
-
-    follower = models.ForeignKey(
-        User,
-        related_name="following",
-        on_delete=models.CASCADE
-    )
-
-    following = models.ForeignKey(
-        User,
-        related_name="followers",
-        on_delete=models.CASCADE
-    )
-
-
-
-
-class Notification(models.Model):
-
-    receiver = models.ForeignKey(
-        User,
-        related_name="notifications",
-        on_delete=models.CASCADE
-    )
-
-
-    sender = models.ForeignKey(
-        User,
-        related_name="sent_notifications",
-        on_delete=models.CASCADE
-    )
-
-
-    message = models.CharField(
-        max_length=255
-    )
-
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-
-    is_read = models.BooleanField(
-        default=False
-    )
-
-
-
-
-
-
+ created_at =models.DateTimeField(auto_now_add=True)
